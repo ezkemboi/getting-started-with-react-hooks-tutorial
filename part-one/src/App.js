ae57ff9f36930d8bd67f9f7291109fe69f9983fb
Ezrqn Kemboi <ezrqnkemboi@gmail.com>
2021-02-21 13:18:47 +0300

    add imgs into array

--- part-one/src/App.js
+++ part-one/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import images from './images';
 
 function App() {
   return (
     <div className="App">
       <h3>Learn React Hooks</h3>
+      <img 
+        src={images[0]} 
+        style={{ 
+          height: '60vh', 
+          width: '95vw'
+        }}
+      />
     </div>
   );
 }
---
+++ part-one/src/images.js
@@ -0,0 +1,7 @@
+import one from "./images/one.jpg";
+import two from "./images/two.jpg";
+import three from "./images/three.jpg";
+import four from "./images/four.jpg";
+import five from "./images/five.jpg";
+
+export default [one, two, three, four, five]
